0636af913c3384944b45b545d3f2acbf4b15bf75
LaniMarsh <lmarsh12@calpoly.edu>
2024-10-05 21:06:34 -0700

    Implemented verify()

--- cbc.py
+++ cbc.py
@@ -38,6 +38,10 @@ def pad(data: bytes) -> bytes:
     padding = BLOCK_SIZE - (len(data) % BLOCK_SIZE)
     return data + bytes([padding] * padding)
 
+def unpad(data: bytes) -> bytes:
+    padding = data[-1]
+    return data[:-padding]
+
 def submit(input: str, key: bytes, iv: bytes) -> bytes:
     encoded_input = urllib.parse.quote(input, safe='')
     string = f"userid=456;userdata={encoded_input};session-id=31337"
@@ -46,3 +50,22 @@ def submit(input: str, key: bytes, iv: bytes) -> bytes:
     cipher = AES.new(key, AES.MODE_ECB)
     encrypted_blocks = encrypt(blocks, cipher, iv)
     return b''.join(encrypted_blocks)
+
+def verify(ciphertext: bytes, key: bytes, iv: bytes) -> bool:
+    blocks = [ciphertext[i:i+BLOCK_SIZE] for i in range(0, len(ciphertext), BLOCK_SIZE)]
+    cipher = AES.new(key, AES.MODE_ECB)
+    decrypted_blocks = decrypt(blocks, cipher, iv)
+    decrypted = b''.join(decrypted_blocks)
+    decrypted = unpad(decrypted).decode()
+    if ";admin=true" in decrypted:
+        return True
+    return False
+
+if __name__ == "__main__":
+    key = os.urandom(BLOCK_SIZE)
+    iv = os.urandom(BLOCK_SIZE)
+    user_data = "You're the man now, dog;admin=true"
+    ciphertext = submit(user_data, key, iv)
+    print(ciphertext)
+    cipher = verify(ciphertext, key, iv)
+    print(cipher)
